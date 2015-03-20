58a482a2537a07046050f6dd9fcdb3f85c3005dc
Andrzej Byczko <veriservus@gmail.com>
2015-03-20 19:14:41 +0000

    convert to array before matching

--- spec/range_splitter_spec.rb
+++ spec/range_splitter_spec.rb
@@ -8,12 +8,12 @@ describe 'Range Splitter' do
 
   it 'splits a range into an array of ranges' do
     (1..10).each do |i|
-      (1..10).split(:into => i).size.should == i
+      (1..10).split(:into => i).to_a.size.should == i
     end
   end
 
   it 'splits into 2 by default' do
-    (1..10).split.size.should == 2
+    (1..10).split.to_a.size.should == 2
   end
 
   it 'raises an ArgumentError if given less than 0' do
@@ -27,37 +27,37 @@ describe 'Range Splitter' do
   end
 
   it 'returns an array of self if given 1' do
-    (1..10).split(:into => 1).should == [1..10]
-    (3..8).split(:into => 1).should == [3..8]
+    (1..10).split(:into => 1).to_a.should == [1..10]
+    (3..8).split(:into => 1).to_a.should == [3..8]
   end
 
   it 'places the larger ranges at the beginning' do
-    (1..9).split.should == [1..5, 6..9]
-    (1..11).split.should == [1..6, 7..11]
-    (5..8).split(:into => 3).should == [5..6, 7..7, 8..8]
+    (1..9).split.to_a.should == [1..5, 6..9]
+    (1..11).split.to_a.should == [1..6, 7..11]
+    (5..8).split(:into => 3).to_a.should == [5..6, 7..7, 8..8]
   end
 
   it 'splits as evenly as possible' do
-    (1..10).split(:into => 4).should == [1..3, 4..6, 7..8, 9..10]
-    (1..13).split(:into => 4).should == [1..4, 5..7, 8..10, 11..13]
+    (1..10).split(:into => 4).to_a.should == [1..3, 4..6, 7..8, 9..10]
+    (1..13).split(:into => 4).to_a.should == [1..4, 5..7, 8..10, 11..13]
   end
 
   it 'supports negative ranges' do
-    (-10..-1).split.should == [-10..-6, -5..-1]
-    (-9..-1).split.should == [-9..-5, -4..-1]
-    (-3..7).split(:into => 3).should == [-3..0, 1..4, 5..7]
+    (-10..-1).split.to_a.should == [-10..-6, -5..-1]
+    (-9..-1).split.to_a.should == [-9..-5, -4..-1]
+    (-3..7).split(:into => 3).to_a.should == [-3..0, 1..4, 5..7]
   end
 
   it 'can return fewer elements than the given argument' do
-    (1..4).split(:into => 10).should == [1..1, 2..2, 3..3, 4..4]
-    (-3..-2).split(:into => 3).should == [-3..-3, -2..-2]
-    (-1..1).split(:into => 7).should == [-1..-1, 0..0, 1..1]
+    (1..4).split(:into => 10).to_a.should == [1..1, 2..2, 3..3, 4..4]
+    (-3..-2).split(:into => 3).to_a.should == [-3..-3, -2..-2]
+    (-1..1).split(:into => 7).to_a.should == [-1..-1, 0..0, 1..1]
   end
 
   it 'packs from the end of the array if an optional parameter is given' do
-    (1..9).split(:endianness => :little).should == [1..4, 5..9]
-    (1..11).split(:endianness => :little).should == [1..5, 6..11]
-    (5..8).split(:into => 3, :endianness => :little).should == [5..5, 6..6, 7..8]
+    (1..9).split(:endianness => :little).to_a.should == [1..4, 5..9]
+    (1..11).split(:endianness => :little).to_a.should == [1..5, 6..11]
+    (5..8).split(:into => 3, :endianness => :little).to_a.should == [5..5, 6..6, 7..8]
   end
 
   it 'raises an argument error if the endianness is neither :little nor :big' do
